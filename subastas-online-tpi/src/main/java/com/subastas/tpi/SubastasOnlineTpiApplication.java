bcbdb12abc6b7f238bd6f19046bf4aac929e13ec
luca <lucaescudero06@gmail.com>
2026-06-12 15:39:16 -0300

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ subastas-online-tpi/src/main/java/com/subastas/tpi/SubastasOnlineTpiApplication.java
@@ -0,0 +1,13 @@
+package com.subastas.tpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubastasOnlineTpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubastasOnlineTpiApplication.class, args);
+	}
+
+}
