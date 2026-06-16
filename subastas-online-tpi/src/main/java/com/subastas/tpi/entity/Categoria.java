d25774af56177698f26d9c3995e7c37763d8ae09
Luca-Escudero <luca.escudero2.0@gmail.com>
2026-06-16 16:37:54 -0300

    .

--- subastas-online-tpi/src/main/java/com/subastas/tpi/entity/Categoria.java
+++ subastas-online-tpi/src/main/java/com/subastas/tpi/entity/Categoria.java
@@ -1,5 +1,19 @@
 package com.subastas.tpi.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categorias")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
+    
 
 }
